a22c0d168bfa65bd0ab7c9448997195f8a674e1c
iamanatolich <alex-goncharov@inbox.ru>
2020-11-19 14:15:58 +0300

    Initial commit

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=jobefko',
-    'username' => 'alexey',
-    'password' => 'Gfhjkm45',
+    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'username' => 'root',
+    'password' => '',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
